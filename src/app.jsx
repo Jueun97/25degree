f7218d439f2c9d417d610f15b3286af4e5eda972
KIMYEEUN22 <kye9702@naver.com>
2021-07-24 22:51:09 +0900

    app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,30 @@
-import "./app.css";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import styles from "./app.module.css";
+import Header from "./component/header/header";
+import Section from "./component/section/section";
+import Inform from "./component/user_inform/inform";
 
-function App() {
-  return <h1>Hello</h1>;
+function App({ authService, getData }) {
+  const [city, setCity] = useState("location");
+  const changeCity = (cityData) => {
+    setCity(cityData);
+  };
+  return (
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Header authService={authService} changeCity={changeCity} />
+            <Section getData={getData} city={city} />
+          </Route>
+          <Route exact path="/login">
+            <Inform authService={authService} getData={getData} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
