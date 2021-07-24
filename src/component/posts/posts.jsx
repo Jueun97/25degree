9d9e3149274b923c07b358490da561a01d7938c8
Jueun <zxnm0901@naver.com>
2021-07-25 01:25:19 +0900

    posts

--- src/component/posts/posts.jsx
+++ src/component/posts/posts.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
 import styles from './posts.module.css';
 import Post from '../post/post';
 
-const Posts = ({posts,userId}) => {
+const Posts = ({ posts, userId }) => {
+    const location = useLocation();
+  
     return (
         <section className={styles.posts}>
             {Object.keys(posts).map(key=><Post key={key} post={posts[key]} userId={userId}></Post>)}
