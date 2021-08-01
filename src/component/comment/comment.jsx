62f21a8684a61895add4447ef4c3016378772f58
Jueun <zxnm0901@naver.com>
2021-08-01 22:51:40 +0900

    post css

--- src/component/comment/comment.jsx
+++ src/component/comment/comment.jsx
@@ -5,7 +5,7 @@ const Comment = ({comment}) => {
         <div className={styles.comment}>
             <img className={styles.image} src={process.env.PUBLIC_URL + '/images/react.png'} alt="'" />
 {/*             <span className={styles.user}>@Jueun97</span> */}
-            <span className={styles.message}><b>{comment.writer}</b>{comment.description}</span>
+            <span className={styles.message}><b>{comment.writer}</b>&nbsp;{comment.description}</span>
         </div>    
     )
 };
